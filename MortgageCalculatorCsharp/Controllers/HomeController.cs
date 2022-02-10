3441e9c859df246732617fa5d4841dcfca5713ad
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-02-10 01:31:02 -0800

    Finished Mortgage Calucator Project fixed typos fixed styles for thead to display the color once loan amount is subbmited. Refactor Code on return and code blocks private/public class

--- MortgageCalculatorCsharp/Controllers/HomeController.cs
+++ MortgageCalculatorCsharp/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using MortgageCalculatorCsharp.Helpers;
 
 namespace MortgageCalculatorCsharp.Controllers
 {
@@ -33,7 +34,34 @@ namespace MortgageCalculatorCsharp.Controllers
             return View();
         }
 
+        public IActionResult App()
+        {
 
+            Loan loan = new();
+
+            loan.Payment = 0.0m;
+            loan.TotalInterest = 0.0m;
+            loan.TotalCost = 0.0m;
+            loan.Rate = 3.5m;
+            loan.Amount = 15000m;
+            loan.Term = 60;
+
+            return View(loan);
+
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+            public IActionResult App(Loan loan)
+        {
+            //Calculate the Loan and get the payments
+            var loanHelper = new LoanHelper();
+
+            Loan newloan = loanHelper.GetPayments(loan);
+            
+
+            return View(newloan);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- MortgageCalculatorCsharp/Models/LoanPayment.cs
+++ MortgageCalculatorCsharp/Models/LoanPayment.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CSSharrMortgageCalc.Models
+namespace MortgageCalculatorCsharp.Models
 {
     public class LoanPayment
     {
